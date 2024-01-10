80a2c340593f8011b1220f1b6effb6d37ccf2c50
Tony Blard <tonyblard55@gmail.com>
2024-01-10 14:59:55 +0100

    Feat : Use TodosList in TodosPage

--- src/pages/TodosPage.tsx
+++ src/pages/TodosPage.tsx
@@ -1,7 +1,12 @@
+import TodosList from "../components/Todos/TodosList";
+
+
 const TodosPage: React.FC = () => {
   return (
     <>
       <h1 className="text-3xl font-bold text-center">Vos listes</h1>
+
+      <TodosList  />
     </>
   );
 };
